e600a155e108aae386458f070547ec36b0ccd18b
sead <sead.softic@gmail.com>
2022-07-25 22:50:37 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            
+<div>
+    
+</div>
+        </div>
+    </div>
+</x-layout>
